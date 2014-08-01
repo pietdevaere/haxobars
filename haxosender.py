a05ddcf321f29258088e1176d4c970fbaa00ed48
Koen Martens <gmc@sonologic.nl>
2014-08-01 03:13:37 +0200

    Fixed a bug introduced by Piet de Vaere
    Piet de Vaere implemented the walker with a single-segment width whereas
    it should be 2 wide and have a 1-wide border at each side which is a mix
    of the background and foreground.

--- haxosender.py
+++ haxosender.py
@@ -108,15 +108,19 @@ if __name__ == "__main__":
     camp = Camp('31.22.122.55')
     for i in range(20):
         newLight = camp.add_light('bar', 1 + i*19, i*4)
+    k=0
     while True:
         for j in range(20):
             for light in camp.lights:
                 light.rgba((0, 0, 255, 0))
             for i in range(80):
                 if i % 20 == j:
-                    camp.set_light(i, (255, 0, 0, 0))
+                    camp.set_light(i, (127, 0, 255, 0))
+                    camp.set_light((i+1)%80, (255,0,0,0))
+                    camp.set_light((i+2)%80, (255,0,0,0))
+                    camp.set_light((i+3)%80, (127,0,255,0))
             camp.transmit()
-            sleep(0.01)
+            sleep(0.005)
         
 
 
